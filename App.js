2eb729264ba1ea8ee3cbd2e2f75f96851bd94cbd
Subrat Mishra <hm979559@gmail.com>
2020-03-11 12:03:56 +0530

    ""

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, Image, StyleSheet } from 'react-native'
 
 export default class App extends Component {
-  render() {
-    return (
-      <View>
-        <Text> textInComponent </Text>
-      </View>
-    )
-  }
+        render() {
+                return (
+                        <View>
+                                <View style={styles.imageview}>
+                                        <Image style={styles.image} source={require('./money.gif')}
+                                        />
+                                </View>
+                        </View>
+                )
+        }
 }
+
+const styles = StyleSheet.create({
+        image: {
+                width: 200,
+                height: 200,
+                alignSelf: "center"
+        },
+        imageview: {
+                flex: 1,
+
+        }
+})
